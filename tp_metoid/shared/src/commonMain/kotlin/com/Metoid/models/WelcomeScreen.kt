9af1de8c08e168112e13d10937b96059a3965d00
TimotheeRiou <timo.riou@gmail.com>
2024-01-10 18:55:54 +0100

    maj interface

--- tp_metoid/shared/src/commonMain/kotlin/com/Metoid/models/WelcomeScreen.kt
+++ tp_metoid/shared/src/commonMain/kotlin/com/Metoid/models/WelcomeScreen.kt
@@ -26,8 +26,14 @@ import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.graphics.BlendMode.Companion.Color
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.withStyle
 import com.Metoid.network.fetchWeatherData
 import io.ktor.client.plugins.ClientRequestException
 import kotlinx.coroutines.launch
@@ -40,68 +46,110 @@ internal fun WelcomeScreen() {
     var errorMessage by remember { mutableStateOf<String?>(null) }
     val coroutineScope = rememberCoroutineScope()
     var isLoading by remember { mutableStateOf(false) }
+    val white = Color(0xFFFFFFFF)
+    val customGreen = Color(0xFF4F6F52)
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = white
     ) {
-        Spacer(modifier = Modifier.height(50.dp)) // Espacement en haut
-
-        Text(
-            "Metoid",
-            fontSize = 30.sp,
-        )
-        Spacer(modifier = Modifier.height(10.dp)) // Espacement entre le titre et le sous-titre
-
-        Text(
-            "La meilleure application météo",
-            fontSize = 20.sp,
-        )
-        Spacer(modifier = Modifier.height(20.dp)) // Espacement entre le sous-titre et la barre de recherche
-
-        TextField(
-            value = searchText,
-            onValueChange = { searchText = it },
-            placeholder = { Text("Rechercher une ville") },
-            modifier = Modifier.fillMaxWidth()
-        )
-        Spacer(modifier = Modifier.height(10.dp)) // Espacement entre la barre de recherche et le bouton
-
-        Button(
-            onClick = {
-                coroutineScope.launch {
-                    isLoading = true // Commence le chargement
-                    errorMessage = null
-                    try {
-                        weatherData = fetchWeatherData(searchText)
-                    } catch (e: Exception) {
-                        errorMessage = "Erreur lors de la récupération des données: ${e.message}"
-                    } finally {
-                        isLoading = false // Termine le chargement
-                    }
-                }
-            }
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text("Recherche")
-        }
+            Spacer(modifier = Modifier.height(50.dp))
+
+            Text(
+                "Metoid",
+                fontSize = 30.sp,
+                fontWeight = FontWeight.Bold,
+                color = customGreen
+            )
+            Spacer(modifier = Modifier.height(10.dp))
+
+            Text(
+                "La meilleure application météo",
+                fontSize = 20.sp,
+                color = MaterialTheme.colors.onSurface
+            )
+            Spacer(modifier = Modifier.height(20.dp))
+
+            OutlinedTextField(
+                value = searchText,
+                onValueChange = { searchText = it },
+                placeholder = { Text("Rechercher une ville") },
+                modifier = Modifier.fillMaxWidth()
+            )
+            Spacer(modifier = Modifier.height(10.dp))
+
+            Button(
+                onClick = {
+                    coroutineScope.launch {
+                        isLoading = true // Commence le chargement
+                        errorMessage = null
+                        try {
+                            weatherData = fetchWeatherData(searchText)
+                        } catch (e: Exception) {
+                            errorMessage =
+                                "Erreur lors de la récupération des données"
+                            //"Erreur lors de la récupération des données: ${e.message}"
+                        } finally {
+                            isLoading = false // Termine le chargement
+                        }
+                    }
+                },
+                shape = RoundedCornerShape(20),
+                colors = ButtonDefaults.buttonColors(backgroundColor = customGreen)
+            ) {
+                Text(
+                    "Recherche",
+                    color = white
+                )
+            }
+            Spacer(modifier = Modifier.height(20.dp))
 
 
-        if (isLoading) {
-            CircularProgressIndicator()
-        }
+            if (isLoading) {
+                CircularProgressIndicator()
+            }
 
-        weatherData?.let { data ->
-            Text("Météo pour ${data.name}")
-            Text("Température : ${data.main.temp}°C")
-            Text("Humidité : ${data.main.humidity}%")
-            Text ("Description : ${data.weather[0].description}")
-        }
+            weatherData?.let { data ->
+                Text(buildAnnotatedString {
+                    append("Météo pour ")
+                    withStyle(style = SpanStyle(fontWeight = FontWeight.Bold)) {
+                        append("${data.name}, ${data.sys.country}")
+                    }
+                })
+
+                Text(buildAnnotatedString {
+                    append("Température : ")
+                    withStyle(style = SpanStyle(fontWeight = FontWeight.Bold)) {
+                        append("${data.main.temp}°C")
+                    }
+                })
+
+                Text(buildAnnotatedString {
+                    append("Humidité : ")
+                    withStyle(style = SpanStyle(fontWeight = FontWeight.Bold)) {
+                        append("${data.main.humidity}%")
+                    }
+                })
+
+                Text(buildAnnotatedString {
+                    append("Description : ")
+                    withStyle(style = SpanStyle(fontWeight = FontWeight.Bold)) {
+                        append(data.weather[0].description)
+                    }
+                })
+            }
 
 
-        errorMessage?.let {
-            Text(it)
+            errorMessage?.let {
+                Text(it)
+            }
         }
     }
 }
+
---
+++ tp_metoid/shared/src/commonMain/kotlin/com/Metoid/network/MapServiceApi.kt
@@ -0,0 +1,10 @@
+/*package com.Metoid.network
+
+object MapServiceApi {
+    const val MAPBOX_ACCESS_TOKEN = "pk.eyJ1IjoidGltb3RoZWVyaW91IiwiYSI6ImNscjd5aHpnczJwYW8yc3BudDl6NWV1cjgifQ.8TXpXPZ6KdEo8bxCXeEPCQ"
+
+    fun initMapbox(context: Context) {
+        Mapbox.getInstance(context, MAPBOX_ACCESS_TOKEN)
+    }
+}
+*/

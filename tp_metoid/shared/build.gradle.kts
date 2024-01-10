b9a80fdabc5e6f01a1722c0f457ed47a7fb71e06
TimotheeRiou <timo.riou@gmail.com>
2024-01-10 19:00:30 +0100

    add build.gradle.kts

--- tp_metoid/shared/build.gradle.kts
+++ tp_metoid/shared/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     kotlin("multiplatform")
     id("com.android.library")
     id("org.jetbrains.compose")
+    kotlin("plugin.serialization") version "1.9.10"
 }
 
 kotlin {
@@ -28,6 +29,13 @@ kotlin {
                 implementation(compose.material)
                 @OptIn(org.jetbrains.compose.ExperimentalComposeLibrary::class)
                 implementation(compose.components.resources)
+                implementation("io.ktor:ktor-client-core:2.3.4") // core source of ktor
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3") // For making asynchronous calls
+                implementation("io.ktor:ktor-client-content-negotiation:2.3.4") // Simplify handling of content type based deserialization
+                implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.4") // make your dataclasses serializable
+                implementation("io.ktor:ktor-client-json:2.3.4")
+                implementation( "io.ktor:ktor-client-serialization:2.3.4")
+
             }
         }
         val androidMain by getting {
@@ -35,6 +43,9 @@ kotlin {
                 api("androidx.activity:activity-compose:1.7.2")
                 api("androidx.appcompat:appcompat:1.6.1")
                 api("androidx.core:core-ktx:1.10.1")
+                implementation("io.ktor:ktor-client-android:2.3.4") // for Android
+                implementation("com.mapbox.mapboxsdk:mapbox-android-sdk:9.7.0")
+
             }
         }
         val iosX64Main by getting
@@ -45,10 +56,14 @@ kotlin {
             iosX64Main.dependsOn(this)
             iosArm64Main.dependsOn(this)
             iosSimulatorArm64Main.dependsOn(this)
+            dependencies {
+                implementation("io.ktor:ktor-client-darwin:2.3.4")
+            }
         }
         val desktopMain by getting {
             dependencies {
                 implementation(compose.desktop.common)
+                implementation("io.ktor:ktor-client-apache:2.3.4") // for Desktop
             }
         }
     }
@@ -72,4 +87,7 @@ android {
     kotlin {
         jvmToolchain(17)
     }
+}
+dependencies {
+    implementation("androidx.compose.ui:ui-text-android:1.5.4")
 }
